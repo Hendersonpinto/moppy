6a1ef80e4376836eccbed058f1726e7d866ec92a
Henderson Pinto <hendersoncode@gmail.com>
2020-04-20 00:52:19 +0200

    generating devise for cleaners and hosts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # devise_for :cleaners
+  # devise_for :hosts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root to: "pages#home"
+
+  devise_for :cleaners, path: 'cleaners', controllers: { sessions: "cleaners/sessions"}
+  devise_for :hosts, path: 'hosts', controllers: { sessions: "hosts/sessions"}
+
 end
