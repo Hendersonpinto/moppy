60822434ab6ad3264d6d1413753400dbb43e3552
Henderson Pinto <hendersoncode@gmail.com>
2020-05-06 21:17:18 +0200

    Migrating from class based component system to functional components with hooks

---
+++ app/javascript/components/LogForm copy.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  FormFeedback,
+} from "reactstrap";
+
+class LogForm extends React.Component {
+  renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return { valid: false, invalid: true };
+    }
+    if (touched) {
+      return { valid: true, invalid: false };
+    }
+    return { valid: null, invalid: null };
+  };
+
+  renderInput = (formProps) => {
+    return (
+      <FormGroup check={formProps.check ? true : null}>
+        <Label check={formProps.check ? true : null}>{formProps.label}</Label>
+        <Input
+          type={formProps.type}
+          id={formProps.input.name}
+          placeholder={formProps.placeholder}
+          {...formProps.input}
+          autoComplete="off"
+          valid={this.renderError(formProps.meta).valid}
+          invalid={this.renderError(formProps.meta).invalid}
+        />
+        <FormFeedback>{formProps.meta.error}</FormFeedback>
+      </FormGroup>
+    );
+  };
+  renderInputCheck = (formProps) => {
+    return (
+      <FormGroup check={formProps.check ? true : null}>
+        <Label check={formProps.check ? true : null}>
+          {formProps.label}
+          <Input type={formProps.type} />
+        </Label>
+      </FormGroup>
+    );
+  };
+
+  onSubmit = (formValues) => {
+    this.props.onSubmit(formValues);
+  };
+
+renderFormGroups(fields){
+  return fields.map((field)=>{
+    return (
+      <FormGroup key={field.name}>
+      <Field
+        name={field.name}
+        component={this.renderInput}
+        label={field.label}
+        type={field.type}
+        placeholder={field.placeholder}
+        {...field.extraProps}
+      />
+    </FormGroup>
+    )
+  })
+}
+
+
+  render() {
+    console.log(this.props)
+    return (
+      <>
+        <form onSubmit={()=>{this.props.handleSubmit(this.onSubmit)}}>
+          {this.renderFormGroups(this.props.fields)}
+          <p>Forgot email or password ?</p>
+          <FormGroup>
+          <Field
+            name="remember"
+            component={this.renderInputCheck}
+            label="Remember this device"
+            type="checkbox"
+            check={true}
+          />
+          </FormGroup>
+          <Button className="mybutton salmon-button">
+            <div className="google">
+              <img src={this.props.buttonImage} className="lock"></img>
+              <p>Sign In</p>
+            </div>
+          </Button>
+        </form>
+        {this.props.myerror ? (
+          <p>{`Something went wrong: ${this.props.myerror}`}</p>
+        ) : (
+          ""
+        )}
+      </>
+    );
+  }
+}
+
+// formValues is passed as props with the values from the fields
+// when validate is called, with the Key specified on our Field ReactComponent
+const validate = (formValues) => {
+  // object declared to store errors
+  const min_digits = 6;
+  const errors = {};
+  // Only run if user did not enter a title
+  if (!formValues.email) {
+    errors.email = "You must enter an email";
+  }
+  if (!formValues.password) {
+    errors.password = "You must enter a password";
+  }
+  if (formValues.password && formValues.password.length < min_digits) {
+    errors.password = `Your password must contain at least ${min_digits} digits`;
+  }
+  return errors;
+};
+
+export default reduxForm({
+  form: "LogForm",
+  validate,
+})(LogForm);
---
+++ app/javascript/components/forms/logInFields.jsx
@@ -0,0 +1,14 @@
+const logInFields = [{
+    name:"email",
+    label:"Email",
+    type:"email",
+    placeholder:"Insert your email"
+},
+{
+    name:"password",
+    label:"Password",
+    type:"password",
+    placeholder:"Type your password"
+}]
+
+export default logInFields;
