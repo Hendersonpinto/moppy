3f839882f9c285f704abe3da26e03e9a917d7132
Henderson Pinto <hendersoncode@gmail.com>
2020-05-07 21:31:42 +0200

    transformed log forms from class based to FC with redux hooks

--- app/javascript/components/LogContainer.jsx
+++ app/javascript/components/LogContainer.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link, withRouter } from "react-router-dom";
 
 import { logHost } from "../actions/index";
@@ -12,75 +12,73 @@ import GoogleIcon from "../../assets/images/googleIcon.svg";
 import LockIcon from "../../assets/images/lockIcon.svg";
 import Logo from "../../assets/images/logo.svg";
 
-class LogContainer extends React.Component {
-  onSubmit = (formValues) => {
-    this.props.logHost(formValues);
+const LogContainer = (props) => {
+  const myerror = useSelector((state) => {
+    if (state.hosts.error) {
+      return state.hosts.error;
+    }
+    return null;
+  });
+
+  const dispatch = useDispatch();
+  const onSubmit = (formValues) => {
+    dispatch(logHost(formValues));
   };
 
-  render() {
-    return (
-      <div className={`log__wrapper ${this.props.type}`}>
-        <div className={`log__left ${this.props.type}`}>
-          <div className="log__title">
-            <h1>
-              {this.props.type === "login"
-                ? "We'll make it shine..."
-                : "Welcome to moppy!"}
-            </h1>
-          </div>
-          <img
-            src={this.props.type === "login" ? SuperheroIn : SuperheroUp}
-            id="superheroLog"
-            alt="cleaner"
-          />
+  console.log(props);
+  return (
+    <div className={`log__wrapper ${props.type}`}>
+      <div className={`log__left ${props.type}`}>
+        <div className="log__title">
+          <h1>
+            {props.type === "login"
+              ? "We'll make it shine..."
+              : "Welcome to moppy!"}
+          </h1>
         </div>
-        <div className={`log__right ${this.props.type}`}>
-          <div className="myform">
-            <div className="myform__header">
-              <img src={Logo}></img>
-            </div>
-            <h3>Welcome back</h3>
-            <LogForm
-              onSubmit={this.onSubmit}
-              myerror={this.props.myerror}
-              buttonImage={LockIcon}
-            ></LogForm>
-            <div className="separator__wrapper">
-              <div className="separator"></div>
-              <span>or sign in with</span>
-              <div className="separator"></div>
-            </div>
-            <button className="mybutton white-button">
-              <div className="google">
-                <img src={GoogleIcon}></img>
-                <p>Google</p>
-              </div>
-            </button>
-            <Link to="/hosts/sign_up" className="log__home">
-              <p className="formFooter">
-                Don't have an account? <span>Sign Up</span>
-              </p>
-            </Link>
+        <img
+          src={props.type === "login" ? SuperheroIn : SuperheroUp}
+          id="superheroLog"
+          alt="cleaner"
+        />
+      </div>
+      <div className={`log__right ${props.type}`}>
+        <div className="myform">
+          <div className="myform__header">
+            <img src={Logo}></img>
           </div>
-          <Link to="/" className="log__home">
-            <div className="log__home-wrapper">
-              <p>Go to home</p>
-              <img src={HomeIcon}></img>
+          <h3>Welcome back</h3>
+          <LogForm
+            onSubmit={onSubmit}
+            myerror={myerror}
+            buttonImage={LockIcon}
+          ></LogForm>
+          <div className="separator__wrapper">
+            <div className="separator"></div>
+            <span>or sign in with</span>
+            <div className="separator"></div>
+          </div>
+          <button className="mybutton white-button">
+            <div className="google">
+              <img src={GoogleIcon}></img>
+              <p>Google</p>
             </div>
+          </button>
+          <Link to="/hosts/sign_up" className="log__home">
+            <p className="formFooter">
+              Don't have an account? <span>Sign Up</span>
+            </p>
           </Link>
         </div>
+        <Link to="/" className="log__home">
+          <div className="log__home-wrapper">
+            <p>Go to home</p>
+            <img src={HomeIcon}></img>
+          </div>
+        </Link>
       </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  if (state.hosts.error) {
-    return {
-      myerror: state.hosts.error,
-    };
-  }
-  return { myerror: null };
+    </div>
+  );
 };
 
-export default connect(mapStateToProps, { logHost })(withRouter(LogContainer));
+export default withRouter(LogContainer);
